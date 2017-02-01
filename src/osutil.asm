3377b0ba274a5add6d56ad6ab515f3be2ea2a1b9
drdanick <di.da.nick@gmail.com>
2017-02-01 03:13:32 -0800

    Added extra register push/pop routines

--- src/osutil.asm
+++ src/osutil.asm
@@ -20,8 +20,10 @@
 
 ; Function pointers
 .nearptr PUSHREGS
+.nearptr PUSHALLREGS
 .nearptr PUSHREGSANDCOPY
 .nearptr POPREGS
+.nearptr POPALLREGS
 .nearptr HALT
 
 
@@ -53,7 +55,54 @@ PUSHREGS:
     ASET 7
     SPUSH
 
-    BRnzp
+    JMP
+
+;
+; Pushes all registers ($a0-$a15), except for the currently selected register, which is lost.
+; to the stack.
+;
+; Volatile registers:
+; - Currently selected register
+;
+PUSHALLREGS:
+    ; Place the return address in $mr before pushing anything to the stack
+    OS_SYSCALL_RET_PREP
+
+    ASET 0
+    SPUSH
+    ASET 1
+    SPUSH
+    ASET 2
+    SPUSH
+    ASET 3
+    SPUSH
+    ASET 4
+    SPUSH
+    ASET 5
+    SPUSH
+    ASET 6
+    SPUSH
+    ASET 7
+    SPUSH
+    ASET 8
+    SPUSH
+    ASET 9
+    SPUSH
+    ASET 10
+    SPUSH
+    ASET 11
+    SPUSH
+    ASET 12
+    SPUSH
+    ASET 13
+    SPUSH
+    ASET 14
+    SPUSH
+    ASET 15
+    SPUSH
+
+    JMP
+    
 
 ;
 ; Pushes all non-volatile registers ($a0-$a7) 
@@ -119,7 +168,7 @@ PUSHREGSANDCOPY:
     ASET 7
     SPOP
         
-    BRnzp
+    JMP
 
 ;
 ; Pops registers $a0-$a7 from the stack
@@ -147,7 +196,51 @@ POPREGS:
     ASET 0
     SPOP
 
-    BRnzp
+    JMP
+
+;
+; Pops registers $a0-$a15 from the stack
+;
+; Volatile registers:
+; - Currently selected register
+;
+POPALLREGS:
+    OS_SYSCALL_RET_PREP
+
+    ASET 15
+    SPOP
+    ASET 14
+    SPOP
+    ASET 13
+    SPOP
+    ASET 12
+    SPOP
+    ASET 11
+    SPOP
+    ASET 10
+    SPOP
+    ASET 9
+    SPOP
+    ASET 8
+    SPOP
+    ASET 7
+    SPOP
+    ASET 6
+    SPOP
+    ASET 5
+    SPOP
+    ASET 4
+    SPOP
+    ASET 3
+    SPOP
+    ASET 2
+    SPOP
+    ASET 1
+    SPOP
+    ASET 0
+    SPOP
+
+    JMP
 
 ;
 ; Effectively halt the CPU
@@ -159,4 +252,4 @@ HALT:
     AND 0
     ADD 15
     PRTout 0x00
-    BRnzp HALT
+    JMP HALT
