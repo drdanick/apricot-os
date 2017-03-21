3e10d8412a19355e8dd8b4ff79b2ba39be27ee67
drdanick <di.da.nick@gmail.com>
2017-03-20 20:48:01 -0700

    Add disk validity checks. Also shrink codebase to ensure it remains smaller than 257 bytes.

--- src/boot.asm
+++ src/boot.asm
@@ -31,15 +31,13 @@
 ;
 ; Volatile:
 ; Alters $mar
-#macro CLS treg {
-    ASET treg
+#macro CLS {
     LARl 0x12
     PRTout 7
     PRTout 7
 }
 
-#macro TTY_MODE mode treg {
-    ASET treg
+#macro TTY_MODE mode {
     LARl mode
     PRTout 7
 }
@@ -51,8 +49,8 @@
 ;==                                     ==
 ;=========================================
 ;TODO: TTY should not have the cursor invisible by default
-TTY_MODE 0x03 0 ; End any previous TTY command
-CLS 0           ; Clear screen
+TTY_MODE 0x03   ; End any previous TTY command
+CLS             ; Clear screen
 
 ; Use $a14 to hold the sleep period (we will use 1000ms, so $a14 must hold 100)
 ASET 14
@@ -61,14 +59,12 @@ LARl 100
 ; Select first disk and try to read.
 ; Give up on 3rd attempt and print error
 
-ASET 1 ; $a1 will hold a negation of the counter we want (-3)
-AND 0
-ADD 2
-NOT    ; Binary NOT of 2 is -3
+ASET 1    ; $a1 will hold a negation of the counter we want (-3)
+LARl 0xFD ; 0xFD is -3
 
 ASET 0  ; Use $a0 to hold the ID of disk 0
 AND 0
-LOAD_DISK: 
+LOAD_DISK:
 ASET 0
 PRTout 0x03
 
@@ -108,9 +104,47 @@ LARl 0x3E
 ; Load sector 0x3E
 PRTout 0x05
 
-; Check that the last 2 bytes of this sector are 0xAA and 0x55 (xoring them together will produce 0xFF)
+; Check that the first 4 bytes of this sector are 0xC0, 0x30, 0x0C, and 0x03 (xoring them together will produce 0xFF)
 ASET 15
 STah
+AND 0
+STal
+
+ASET 10
+LD        ; Load first char
+SPUSH
+LDal
+ADD 1
+STal
+LD        ; Load second char
+SPUSH
+LDal
+ADD 1
+STal
+LD        ; Load third char
+SPUSH
+LDal
+ADD 1
+STal
+LD        ; Load fourth char
+SPOP XOR  ; Pop the other three characters
+SPOP XOR
+SPOP XOR
+ADD 1     ; Adding 1 should make it equal to zero
+
+BRz BOOT_CODE_CHECK
+
+; Disk is not valid
+ASET 8
+LARl DISK_FORMAT_ERROR
+JMP PRINT_ERROR
+
+BOOT_CODE_CHECK:
+
+; Check that the last 2 bytes of this sector are 0xAA and 0x55 (xoring them together will produce 0xFF)
+ASET 15
+LDah
+;STah
 STal   ; Load the first byte
 ASET 10
 LD
@@ -127,8 +161,9 @@ BRz COPY_LOADER
 
 ; Disk is not bootable
 ASET 8
-LARl DISK_FORMAT_ERROR
+LARl DISK_NOT_BOOTABLE_ERROR
 JMP PRINT_ERROR
+
 
 ; Disk is bootable. Copy the first 128 sectors into memory.
 COPY_LOADER:
@@ -153,7 +188,7 @@ PRINT_LOAD_MSG:
     JMP PRINT_LOAD_MSG
 PRINT_LOAD_MSG_END:
 SPOP
-    
+
 
 
 
@@ -171,7 +206,7 @@ LOADER_COPY_LOOP:
         STal                    ; Set sector local address
         ASET 15                 ; Set sector address to disk paging area
         STah
-        ASET 4                  ; Temporary register for loading values         
+        ASET 4                  ; Temporary register for loading values
         LD                      ; Load from the disk paging area
         ASET 0                  ; Set the sector address to the sector we're writing to
         STah
@@ -184,28 +219,14 @@ LOADER_COPY_LOOP:
     ADD 1
 BRnp LOADER_COPY_LOOP
 
-;CLS 0
-
-;ASET 8
-;LAl GOOD
-;LDal
-;BRnzp PRINT_ERROR
-
-; Set the address of the first loaded segment, and jump to it
-;ASET 0
-;AND 0
-;STal
-;ADD 1
-;STah
-
+; Set the address of the first loaded segment, and prepare to jump to it
 LAh 1
 LAl 0
 
 ; Jump to first loaded segment
-;PRTin 4
 JMP
-    
-    
+
+
 
 
 ; Routine to print an error message and halt
@@ -214,7 +235,7 @@ JMP
 ; It is expected that the mar already contains the message segment number
 PRINT_ERROR:
     ; Write 0x7F to the TTY in order to enable line mode
-    ASET 15 
+    ASET 15
     LARl 0x7F
     PRTout 0x07
     AND 0
@@ -233,7 +254,7 @@ PRINT_ERROR:
         JMP PRINT_LOOP
     PRINT_END:
     ; Disable TTY line mode
-    ASET 15 
+    ASET 15
     AND 0
     ADD 3
     PRTout 0x07
@@ -244,9 +265,10 @@ PRINT_ERROR:
     PRTout 0x00
     JMP HALT
 
-    
+
 
 ; Messages
 DISK_READ_ERROR:   .stringz  "Disk read error!"
-DISK_FORMAT_ERROR: .stringz  "Disk 0 is not bootable!"
-LOADING: .stringz "Loading Operating System..."
+DISK_NOT_BOOTABLE_ERROR: .stringz  "Disk 0 is not bootable!"
+DISK_FORMAT_ERROR: .stringz  "Disk 0 is not valid!"
+LOADING: .stringz "Loading ApricotOS..."
