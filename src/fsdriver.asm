89c3332aff483d94ea430bc6c4d46761eacd0354
drdanick <di.da.nick@gmail.com>
2017-03-25 04:43:52 -0700

    Implemented volume mount routine

--- src/fsdriver.asm
+++ src/fsdriver.asm
@@ -16,4 +16,37 @@
 #name "fs"
 #segment 16
 
-NOP
+#include "apricotosint.asm"
+#include "fsmem.asm"
+#include "diskio.asm"
+
+.nearptr MOUNT_DISK
+
+
+;
+; Mount a filesystem on the currently loaded disk
+;
+; Volatile registers:
+; $a8
+;
+MOUNT_DISK:
+    ASET 8
+    DISKIO_TRACKSEL 0
+
+    ; Load the volume information sector into memory (sector 62)
+    LARl 62
+    ASET 9
+    LARh FSMEM_VOL_INFO
+    ASET 10
+    MEOW:
+    OS_SYSCALL DISKIO_COPYFROMDSK
+
+    ; Load the space bitmap sector into memory (sector 63)
+    ASET 8
+    LARl 63
+    ASET 9
+    LARh FSMEM_VOL_BITMAP
+    ASET 10
+    OS_SYSCALL DISKIO_COPYFROMDSK
+
+    OS_SYSCALL_RET
