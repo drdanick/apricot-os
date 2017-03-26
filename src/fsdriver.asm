e4196d50befaa32206fd7e62643bf11f73abe5da
drdanick <di.da.nick@gmail.com>
2017-03-26 04:35:30 -0700

    Added disk validation to mount routine

--- src/fsdriver.asm
+++ src/fsdriver.asm
@@ -27,7 +27,10 @@
 ; Mount a filesystem on the currently loaded disk
 ;
 ; Volatile registers:
-; $a8
+; $a8-$a15
+;
+; Returns:
+; $a8 - Non-zero if the disk was mounted successfully, zero otherwise.
 ;
 MOUNT_DISK:
     ASET 8
@@ -49,4 +52,42 @@ MOUNT_DISK:
     ASET 10
     OS_SYSCALL DISKIO_COPYFROMDSK
 
+    ; Validate the disk by checking that the first 4 bytes of
+    ; this sector are 0xC0, 0x30, 0x0C, and 0x03.
+    ; Xoring them together will produce 0xFF.
+
+    ASET 8 ; Set the return value to be initially non-zero
+    OR 1
+
+    LAh FSMEM_VOL_INFO
+    LAl 0x00
+    ASET 9
+    LD
+    SPUSH
+    LDal
+    ADD 1
+    STal
+    LD
+    SPUSH
+    LDal
+    ADD 1
+    STal
+    LD
+    SPUSH
+    LDal
+    ADD 1
+    STal
+    LD
+    SPOP XOR
+    SPOP XOR
+    SPOP XOR
+    ADD 1 ; Adding 1 will make it zero if the result was 0xFF (The only valid result)
+    BRz MOUNT_DISK_END
+
+    ; Disk was not valid, so set the return value to be zero
+    ASET 8
+    AND 0
+
+    MOUNT_DISK_END:
+    ASET 10
     OS_SYSCALL_RET
