90446a8d6c805e01117f0aceb41010821aad6b94
drdanick <di.da.nick@gmail.com>
2017-03-29 20:38:33 -0700

    Added disk allocation skeleton

--- src/diskalloc.asm
+++ src/diskalloc.asm
@@ -18,4 +18,42 @@
 #name "diskalloc"
 #segment 7
 
-NOP
+.nearptr CHECK_SECTOR
+.nearptr AUTO_ALLOCATE_SECTOR
+
+; Check if a disk sector is free or not.
+; $a8 - The track number of the sector to check
+; $a9 - The sctor number to check
+;
+; Returns:
+; $a8 - Zero if the sector is free, non-zero otherwise
+;
+; Volatile registers:
+; TODO
+CHECK_SECTOR:
+    ; TODO
+    NOP
+
+; Find and allocate the first available sector
+;
+; Returns:
+; $a8  - Non-zero if the operation was successful, zero otherwise
+; $a9  - The track number of the allocated sector
+; $a10 - The sector number of the allocated sector
+;
+; Volatile registers:
+; TODO
+AUTO_ALLOCATE_SECTOR:
+    ; TODO
+    NOP
+
+; Free a given sector
+;
+; $a8 - The track number of the sector to free
+; $a9 - The sector number to free
+;
+; Volatile registers:
+; TODO
+FREE_SECTOR:
+    ; TODO
+    NOP
