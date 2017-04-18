e6d6ff4997d1e84fd8aac3bc974f84c27c6c68b4
drdanick <di.da.nick@gmail.com>
2017-04-18 00:42:02 -0700

    Added missing PUTNUM implementation

--- src/disp.asm
+++ src/disp.asm
@@ -19,6 +19,7 @@
 
 #include "apricotosint.asm"
 #include "portout.asm"
+#include "math.asm"
 
 #macro TTY_MODE mode treg {
     ASET treg
@@ -33,6 +34,7 @@
 
 ; Routine pointers
 .nearptr PUTSTR
+.nearptr PUTNUM
 .nearptr TTYRESET
 
 ; Write a null terminated string to the display
@@ -72,6 +74,61 @@ PUTSTR:
     ASET 8
     OS_SYSCALL_RET
 
+; Write an 8 bit number to the display
+; $a8 - The number to write
+;
+; Volatile registers:
+; $a8
+; $a9
+; $a10
+; $a11
+PUTNUM:
+    ASET 9
+    LARl 100
+    ASET 10
+    OS_SYSCALL LIBMATH_DIV
+
+    ASET 10
+    BRz PUTNUM_SKIP_100
+    SPUSH
+    LARl DECTOCHAR_TABLE
+    SPOP ADD
+    STal
+    LAh DECTOCHAR_TABLE
+    LD
+    PORTOUT_TTY_WRITE
+    PUTNUM_SKIP_100:
+
+    ASET 11
+    SPUSH
+    ASET 8
+    SPOP
+    ASET 9
+    LARl 10
+    ASET 10
+    OS_SYSCALL LIBMATH_DIV
+
+    ASET 10
+    BRz PUTNUM_SKIP_10
+    SPUSH
+    LARl DECTOCHAR_TABLE
+    SPOP ADD
+    STal
+    LAh DECTOCHAR_TABLE
+    LD
+    PORTOUT_TTY_WRITE
+    PUTNUM_SKIP_10:
+
+    ASET 11
+    SPUSH
+    LARl DECTOCHAR_TABLE
+    SPOP ADD
+    STal
+    LAh DECTOCHAR_TABLE
+    LD
+    PORTOUT_TTY_WRITE
+
+    OS_SYSCALL_RET
 
 ; Reset the TTY
 ;
